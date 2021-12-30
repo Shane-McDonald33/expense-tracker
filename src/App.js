2e3a895aac411ba6c62e21d6f1c88414ce683cc0
aydenapollo <mcdonaldshane33@gmail.com>
2021-12-29 22:34:55 -0800

    practicing with props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import ExpenseItem from "./Components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Skin Care Routine',
+      amount: 202.39,
+      date: new Date(2021, 11, 29),
+    },
+    {
+      id: 'e2',
+      title: 'Phone Bill',
+      amount: '45.35',
+      date: new Date(2021, 9, 10),
+    },
+    {
+      id: 'e3',
+      title: 'Monitor',
+      amount: 369.32,
+      date: new Date(2021, 9, 13),
+    },
+    {
+      id: 'e4',
+      title: 'Shoes',
+      amount: 78.47,
+      date: new Date(2021, 10, 24)
+    }
+  ]
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem/>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
     </div>
   );
 }
--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -1,19 +1,21 @@
+import React from 'react';
 import './ExpenseItem.css'
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+    
     return (
         <div className='expense-item'>
             <div>
-                12/29/2021
+                {props.date.toISOString()}
             </div>
             <div className='expense-item__description'>
-                <h2>Skin Care Routine</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>
-                    $202.39
+                    ${props.amount}
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default ExpenseItem
